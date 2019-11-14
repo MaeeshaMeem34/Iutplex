93211f864511ad1a1c81db9e941421b3f7b4cab5
Anirban Tasfin Azad <47144252+Anirban-45@users.noreply.github.com>
2019-11-14 20:24:17 +0600

    checked
    done

---
+++ Project_Iutplex/storage/framework/views/25897227313a86bc3d98350b5197ee8359317e06.php
@@ -0,0 +1,13 @@
+<?php $__env->startSection('title'); ?>
+   About
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+   <h1>About us</h1>
+   <hr>
+   <p>
+       qui officia deserunt mollit anim id est laborum.
+   </p>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Laravel\Iutplex\Project_Iutplex\resources\views/About.blade.php ENDPATH**/ ?>
